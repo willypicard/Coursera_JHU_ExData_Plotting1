665267ace72f4f3a880096235f895311e3ac98f2
Willy Picard <willy.picard@currency-one.com>
2014-05-09 00:26:02 +0200

    - added support for data download/unzip

--- code/loadData.R
+++ code/loadData.R
@@ -1,6 +1,21 @@
 rm(list = ls())
+DATA_DIR = "../data"
+UNZIPPED_FILE_DATA = "household_power_consumption.txt"
+FILE_DATA = paste(DATA_DIR, UNZIPPED_FILE_DATA, sep="/")
+ZIP_FILE_DATA = paste(FILE_DATA, "zip", sep = ".")
+URL_DATA = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+old_wd = getwd()
+setwd(DATA_DIR)
+if (!file.exists(UNZIPPED_FILE_DATA)){
+  download.file(URL_DATA, ZIP_FILE_DATA)
+  
+  unzip(ZIP_FILE_DATA)
+  file.remove(ZIP_FILE_DATA)
+}
+setwd(old_wd)
+
 data_all = read.csv("../data/household_power_consumption.txt", sep = ";", na.strings = "?")
 data = subset(data_all, Date == "1/2/2007" | Date == "2/2/2007")
 rm (data_all)
 data$Date2 = strptime(paste(data$Date, data$Time), "%d/%m/%Y %X")
-head(data)
